fc12b971603d9c11eb1849838500e4fa9f0668c0
henrycccc <henry810415@gmail.com>
2020-12-23 10:52:37 +0800

    test3

--- src/main/java/com/web/game/exchange/controller/ResultController.java
+++ src/main/java/com/web/game/exchange/controller/ResultController.java
@@ -16,8 +16,8 @@ public class ResultController {
 				@RequestParam(value="action")String action,
 				@RequestParam(value="path")String path,
 				Model model) {//path決定導向Thanks or Fail
+			System.out.println("???????????????????????????");
 			model.addAttribute("action",action);
-			System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 		return "/exchange/"+path;
 		}
 }
