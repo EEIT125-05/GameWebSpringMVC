73ae7246ee8fd10d92ca818851928f82fd054336
henrycccc <76465049+henrycccc@users.noreply.github.com>
2021-01-20 00:53:29 +0800

    202101201300

--- src/main/java/com/web/game/withplay/controller/NocheckWithController.java
+++ src/main/java/com/web/game/withplay/controller/NocheckWithController.java
@@ -1,5 +1,6 @@
 package com.web.game.withplay.controller;
 
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withBadRequest;
 
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
@@ -28,6 +29,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.web.game.contest.service.GameListService;
+import com.web.game.home.aspect.userAspect;
 import com.web.game.member.model.MemberBean;
 import com.web.game.withplay.model.WithOrder;
 import com.web.game.withplay.model.WithPlay;
@@ -56,6 +58,7 @@ public class NocheckWithController {
 	
 	@GetMapping("/withplay/Index")
 	public String WithplayIndex(Model model) {
+//		model.addAttribute("Withsearch", withService.search("", ""));
 		model.addAttribute("Withlist",withService.list());
 		model.addAttribute("GameList",ListService.selectGameList());		
 		if(model.getAttribute("user")!=null) {
